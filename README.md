9355b652c71478489d331b1ecb963d45d4422254
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2022-01-05 19:48:58 -0300

    Readme changes

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # SGB Emulator
 
-## Build
+## Build & Execution
+
+Create cmake environment
 
 ```
     mkdir build
     cd build/
     cmake ..
+```
+
+Compile
+```
+    cd build/
+    make
+```
+
+Execution
+```
+    cd executable/
+    ./sgb-emu
 ```
--- executable/main.cpp
+++ executable/main.cpp
@@ -4,15 +4,7 @@
 
 int main()
 {
-
    Bus barramento = Bus();
-
-   std::cout << "Olá 123" << std::endl;
-
-
-   std::cout << "Addr Bus: " << &barramento << std::endl;
-   std::cout << "Addr Bus: " << barramento.cpu.bus << std::endl;
-   std::cout << "Addr Bus: " << barramento.cpu.cb.bus << std::endl;
 
    return 0;
 }
