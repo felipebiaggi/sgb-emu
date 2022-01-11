b8427b312f59135a908dd54ff805fbbe7feb6247
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2022-01-10 21:37:49 -0300

    Create cartridge header

---
+++ cartridge/src/cartridge.h
@@ -0,0 +1,21 @@
+#ifndef CARTRIDGE_H
+#define CARTRIDGE_H
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+
+class Cartridge {
+public:
+    Cartridge(vector<uint8_t> game_data);
+    ~Cartridge();
+
+
+private:
+    
+
+};
+
+
+#endif
